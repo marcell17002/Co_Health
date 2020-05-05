d6709ceab393bdd17755b0df74d3fe00568c3c6e
Firmansyah Yanuar <fyfirman@gmail.com>
2020-05-05 23:40:23 +0700

    fix navbar icon

--- src/components/molecules/Navbar/index.js
+++ src/components/molecules/Navbar/index.js
@@ -6,7 +6,7 @@ const Menu = (props) => {
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <TouchableOpacity onPress={props.OnPress}>
         <View style={{ width: 26, height: 26, alignItems: 'center' }}>
-          <Image source={props.img} style={{ marginLeft: 20, alignItems: 'center', justifyContent: 'center' }} />
+          <Image source={props.img} style={{ alignItems: 'center', justifyContent: 'center', width: 40, height: 26 }} />
         </View>
         <Text style={{ fontSize: 10, color: '#545454', marginTop: 4, textAlign: 'center' }}>{props.tittle}</Text>
       </TouchableOpacity>
