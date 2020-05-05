ad1c3d5d406c044329e867a4d0f9cfa9ffda6fd9
Firmansyah Yanuar <fyfirman@gmail.com>
2020-05-05 23:05:49 +0700

    fix formatting

--- src/config/router/index.js
+++ src/config/router/index.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Calendara, CreateTask , Landing , Profile, EditProfile, Login, Register, Scan_qr,Intro}  from '../../containers/pages/';
-
+import { Calendara, CreateTask, Landing, Profile, EditProfile, Login, Register, Scan_qr, Intro } from '../../containers/pages/';
 
 const HomeStack = createStackNavigator();
 const PreStack = createStackNavigator();
@@ -11,50 +10,50 @@ const CalendarStack = createStackNavigator();
 const ProfileStack = createStackNavigator();
 const Stack = createStackNavigator();
 
-function PreStackScreen () {
-  return(
-  <PreStack.Navigator screenOptions={{gestureEnabled: false }} >
-    {/* <PreStack.Screen name="Intro" component={Intro}  headerMode = 'none' screenOptions={{headerShown: false ,gestureEnabled: false }}/> */}
-    <PreStack.Screen name="Login" component={Login}  headerMode = 'none' screenOptions={{headerShown: false ,gestureEnabled: false }}/>
-    <PreStack.Screen name="Register" component={Register}  headerMode = 'none' screenOptions={{headerShown: false ,gestureEnabled: false }}/>
- </PreStack.Navigator>
+function PreStackScreen() {
+  return (
+    <PreStack.Navigator screenOptions={{ gestureEnabled: false }} >
+      {/* <PreStack.Screen name="Intro" component={Intro}  headerMode = 'none' screenOptions={{headerShown: false ,gestureEnabled: false }}/> */}
+      <PreStack.Screen name="Login" component={Login} headerMode='none' screenOptions={{ headerShown: false, gestureEnabled: false }} />
+      <PreStack.Screen name="Register" component={Register} headerMode='none' screenOptions={{ headerShown: false, gestureEnabled: false }} />
+    </PreStack.Navigator>
   );
 }
-function HomeStackScreen () {
-  return(
-  <HomeStack.Navigator screenOptions={{gestureEnabled: false }} >
-    <HomeStack.Screen name="Landing" component={Landing}  headerMode = 'none' screenOptions={{headerShown: false ,gestureEnabled: false }}/>
-    <HomeStack.Screen name="Scan_qr" component={Scan_qr} />
-  </HomeStack.Navigator>
-  );
-}
-
-function CalendarStackScreen () {
-  return(
-  <CalendarStack.Navigator screenOptions={{gestureEnabled: false }} >
-    <CalendarStack.Screen name="Calendar" component={Calendara}/>
-    <CalendarStack.Screen name="CreateTask" component={CreateTask}/>
-  </CalendarStack.Navigator>
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator screenOptions={{ gestureEnabled: false }} >
+      <HomeStack.Screen name="Landing" component={Landing} headerMode='none' screenOptions={{ headerShown: false, gestureEnabled: false }} />
+      <HomeStack.Screen name="Scan_qr" component={Scan_qr} />
+    </HomeStack.Navigator>
   );
 }
 
-function ProfileStackScreen () {
-  return(
-  <ProfileStack.Navigator screenOptions={{gestureEnabled: false }} >
-    <ProfileStack.Screen name="Profile" component={Profile}/>
-    <ProfileStack.Screen name="EditProfile" component={EditProfile}/>
-  </ProfileStack.Navigator>
+function CalendarStackScreen() {
+  return (
+    <CalendarStack.Navigator screenOptions={{ gestureEnabled: false }} >
+      <CalendarStack.Screen name="Calendar" component={Calendara} />
+      <CalendarStack.Screen name="CreateTask" component={CreateTask} />
+    </CalendarStack.Navigator>
+  );
+}
+
+function ProfileStackScreen() {
+  return (
+    <ProfileStack.Navigator screenOptions={{ gestureEnabled: false }} >
+      <ProfileStack.Screen name="Profile" component={Profile} />
+      <ProfileStack.Screen name="EditProfile" component={EditProfile} />
+    </ProfileStack.Navigator>
   );
 }
 
 function App() {
-  return(
+  return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="PreStackScreen" component={PreStackScreen}/>
-        <Stack.Screen name="HomeStackScreen" component={HomeStackScreen}/>
-        <Stack.Screen name="ProfileStackScreen" component={ProfileStackScreen}/>
-        <Stack.Screen name="CalendarStackScreen" component={CalendarStackScreen}/>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="PreStackScreen" component={PreStackScreen} />
+        <Stack.Screen name="HomeStackScreen" component={HomeStackScreen} />
+        <Stack.Screen name="ProfileStackScreen" component={ProfileStackScreen} />
+        <Stack.Screen name="CalendarStackScreen" component={CalendarStackScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
